6f91dcc56a450fe849e38d51399bcd527c2b3a68
reysdaaud <reysdaaud@gmail.com>
2025-06-24 02:12:15 +0300

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors({
   origin: [
-    'http://icasti.com',
+    'https://icasti.com',
     'http://localhost:9002',
     'https://checkout.paystack.com'
   ],
